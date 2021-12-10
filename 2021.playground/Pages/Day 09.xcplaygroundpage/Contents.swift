96577db6e6b98c1761d8cda358d33512c464ee98
Wes Byrne <wesburn@me.com>
2021-12-10 09:13:20 -0800

    Day 9 and Day 10

--- 2021.playground/Pages/Day 09.xcplaygroundpage/Contents.swift
+++ 2021.playground/Pages/Day 09.xcplaygroundpage/Contents.swift
@@ -7,11 +7,59 @@ import AOCUtils
 let data = try! loadData(example: false)
     .trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
     .split(separator: "\n")
+    .map { str in str.map { Int(String($0))! } }
 
-// MARK: - Part 1
-/*-------------------------------------------------------------------------------*/
+struct Point: Hashable {
+    let x: Int
+    let y: Int
+}
+
+func adjacentPoints(to point: Point) -> [Point] {
+    var res = [Point]()
+    if point.x > 0 {
+        res.append(Point(x: point.x - 1, y: point.y))
+    }
+    if point.x < data[point.y].count - 1 {
+        res.append(Point(x: point.x + 1, y: point.y))
+    }
+    if point.y > 0 {
+        res.append(Point(x: point.x, y: point.y - 1))
+    }
+    if point.y < data.count - 1 {
+        res.append(Point(x: point.x, y: point.y + 1))
+    }
+    return res
+}
+
+func value(at point: Point) -> Int {
+    return data[point.y][point.x]
+}
+
+let lowPoints = { () -> [Point] in
+    var res = [Point]()
+    for (yIdx, row) in data.enumerated() {
+        for (xIdx, val) in row.enumerated() {
+            
+            let p = Point(x: xIdx, y: yIdx)
+            let adjescentValues = adjacentPoints(to: p).map {
+                value(at: $0)
+            }
+            let isLowest = !adjescentValues.contains { $0 <= val }
+            if isLowest {
+                res.append(p)
+            }
+        }
+        
+    }
+    return res
+}()
+
+
 run(part: 1) {
-    let result = 0
+    let result = lowPoints
+        .map { value(at: $0) }
+        .map { $0 + 1 }
+        .reduce(0, +)
     return "The result is \(result)"
 }
 
@@ -19,7 +67,26 @@ run(part: 1) {
 // MARK: - Part 1
 /*-------------------------------------------------------------------------------*/
 run(part: 2) {
-    let result = 0
+    var basins = [Int]()
+    
+    for p in lowPoints {
+        var points = Set<Point>()
+        func crawl(from p: Point) {
+            if points.contains(p) { return }
+            if value(at: p) == 9 { return }
+            points.insert(p)
+            for adj in adjacentPoints(to: p) {
+                crawl(from: adj)
+            }
+        }
+        crawl(from: p)
+        basins.append(points.count)
+    }
+    
+    let sorted = basins.sorted().reversed()
+    print(sorted)
+    
+    let result = sorted.prefix(3).reduce(1, *)
     return "The result is \(result)"
 }
 
--- 2021.playground/Pages/Day 10.xcplaygroundpage/Contents.swift
+++ 2021.playground/Pages/Day 10.xcplaygroundpage/Contents.swift
@@ -8,18 +8,75 @@ let data = try! loadData(example: false)
     .trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
     .split(separator: "\n")
 
+let characters = [
+    "(": ")",
+    "[": "]",
+    "{": "}",
+    "<": ">"
+]
+let openingCharacters = Set(characters.keys)
+let closingCharacters = Set(characters.values)
+
 // MARK: - Part 1
 /*-------------------------------------------------------------------------------*/
 run(part: 1) {
-    let result = 0
+    let scores = [
+        ")": 3,
+        "]": 57,
+        "}": 1197,
+        ">": 25137,
+    ]
+    let badElements = data.compactMap { line -> String? in
+        var openElements = [String]()
+        
+        for _c in line {
+            let c = String(_c)
+            if openingCharacters.contains(c) {
+                openElements.append(c)
+            } else if let last = openElements.last, let closer = characters[last], c == closer {
+                openElements.popLast()
+            } else {
+                return c
+            }
+        }
+        return nil
+    }
+    print(badElements)
+    let result = badElements.reduce(0) { res, element in
+        return res + (scores[element] ?? 0)
+    }
     return "The result is \(result)"
 }
 
 
-// MARK: - Part 1
-/*-------------------------------------------------------------------------------*/
 run(part: 2) {
-    let result = 0
+    let scores = [
+        ")": 1,
+        "]": 2,
+        "}": 3,
+        ">": 4,
+    ]
+    let unterminatedLines = data.compactMap { line -> String? in
+        var openElements = [String]()
+        for _c in line {
+            let c = String(_c)
+            if openingCharacters.contains(c) {
+                openElements.append(c)
+            } else if let last = openElements.last, let closer = characters[last], c == closer {
+                openElements.popLast()
+            } else {
+                return nil
+            }
+        }
+        return openElements.reversed().joined()
+    }
+    let results = unterminatedLines
+        .map { completion in
+            completion
+                .map { characters[String($0)]! }
+                .reduce(0, { ($0 * 5) + scores[String($1)]! })
+        }.sorted()
+    
+    let result = results[Int(results.count / 2)]
     return "The result is \(result)"
 }
-
